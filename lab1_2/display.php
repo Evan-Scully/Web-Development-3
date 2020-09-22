65cb18764a95aef01f1b3c6060e85f52bedb7a0e
Evan Scully <48257448+Dawnfang79@users.noreply.github.com>
2020-09-22 17:22:40 +0100

    Lab 2 Completed

--- lab1_2/display.php
+++ lab1_2/display.php
@@ -1,12 +1,31 @@
 <?php
 $product_description = $_POST['product_description'];
-$list_price = $_POST['list price'];
-$discount percentage = $_POST['discount percentage'];
+$list_price = $_POST['list_price'];
+$discount_percentage = $_POST['discount_percentage'];
+
+$discount_amount =  $list_price / $discount_percentage;
+$final_price = $list_price - $discount_amount;
+
 ?>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
 
 <html>
     <body>
-        <p> <?php echo $name; ?> <p>
-        <p> <?php echo $l_name; ?> <p>
+        <div class="content">
+            <h1>Product Discount Calculator</h1>
+            <label>Product Description: </label>
+            <?php echo $product_description; ?></br>
+            <label>List Price: </label>
+            <?php echo "$$list_price"; ?></br>
+            <label>Standard Discount: </label>
+            <?php echo "$discount_percentage%"; ?></br>
+            <label>Discount Amount: </label>
+            <?php echo "$$discount_amount"; ?></br>
+            <label>Discount Price: </label>
+            <?php echo "$$final_price"; ?></br>
+        </div>
     </body>
 </html>
