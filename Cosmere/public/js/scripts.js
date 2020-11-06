cacd752b61a9b23c3b618bd35d60779e34afc939
Evan Scully <evan.scully987@gmail.com>
2020-11-06 19:09:00 +0000

    Image Reshuffle

--- Cosmere/public/js/scripts.js
+++ Cosmere/public/js/scripts.js
@@ -3,8 +3,10 @@ var toggle = false;
 var id = 1;
 var current_series = "";
 var current_book = "";
+
 var series = new Array();
 var updated_series = new Array();
+
 var index = 0;
 
 function findAll(){
@@ -51,55 +53,60 @@ function showOne(data)
 
 function getSeries(series) 
 {
-    updated_series = [];
-    $.each(series,function(index,book) {
-        if(current_series === series[index].series && series[index].name != current_book)
-        {
-            console.log(series[index].name);
-            updated_series.push(series[index]);
-        }
-    });
-    $("#image-1").attr("id",updated_series[0].book_id); 
-    $("#image-2").attr("id",updated_series[1].book_id); 
-    $("#image-3").attr("id",updated_series[2].book_id);
-    
-    //console.log(series[0].image);
-    //console.log(series[1].image);
-    //console.log(series[0].image);
+    jQuery(function(){
+        updated_series = [];
+        index = 0;
+        $.each(series,function(index,book) {
+            if(current_series === series[index].series && series[index].name != current_book)
+            {
+                updated_series.push(series[index]);
+            }
+        });
 
-    $("#series-1").attr("src","images/" + updated_series[0].image + ""); 
-    $("#series-2").attr("src","images/" + updated_series[1].image + "");  
-    $("#series-3").attr("src","images/" + updated_series[2].image + "");   
+        $("#image-1").attr("id",updated_series[0].book_id); 
+        $("#image-2").attr("id",updated_series[1].book_id); 
+        $("#image-3").attr("id",updated_series[2].book_id);
+
+        $("#series-1").attr("src","images/" + updated_series[0].image + ""); 
+        $("#series-2").attr("src","images/" + updated_series[1].image + "");  
+        $("#series-3").attr("src","images/" + updated_series[2].image + "");   
+    });
 }
 
 function moveSeriesRight() {
-    while(index < updated_series.length - 2)
-    {
-        //console.log(series[0].book_id);
-        $("#" + updated_series[2].book_id).attr("id",updated_series[3].book_id); 
-        $("#" + updated_series[1].book_id).attr("id",updated_series[2].book_id); 
-        $("#" + updated_series[0].book_id).attr("id",updated_series[1].book_id); 
-
-        $("#series-1").attr("src","images/" + updated_series[index].image + ""); 
-        $("#series-2").attr("src","images/" + updated_series[index+1].image + "");  
-        $("#series-3").attr("src","images/" + updated_series[index+2].image + "");  
-        index = index + 1;
-        break; 
-    }
+    jQuery(function(){
+        if(index < updated_series.length - 3  && updated_series.length > 3)
+        {
+            $("#" + updated_series[index + 2].book_id).attr("id",updated_series[index + 3].book_id); 
+            $("#" + updated_series[index + 1].book_id).attr("id",updated_series[index + 2].book_id); 
+            $("#" + updated_series[index].book_id).attr("id",updated_series[index + 1].book_id);
+            $("#series-1").attr("src","images/" + updated_series[index + 1].image + ""); 
+            $("#series-2").attr("src","images/" + updated_series[index + 2].image + "");  
+            $("#series-3").attr("src","images/" + updated_series[index + 3].image + ""); 
+            index = index + 1; 
+        }      
+    });
 }
 
 function moveSeriesLeft() {
-    while(index > 1)
-    {
-        $("#" + updated_series[3].book_id).attr("id",updated_series[2].book_id); 
-        $("#" + updated_series[2].book_id).attr("id",updated_series[1].book_id); 
-        $("#" + updated_series[1].book_id).attr("id",updated_series[0].book_id);  
-        $("#series-1").attr("src","images/" + updated_series[index - 2].image + ""); 
-        $("#series-2").attr("src","images/" + updated_series[index - 1].image + "");  
-        $("#series-3").attr("src","images/" + updated_series[index].image + "");  
-        index = index - 1;
-        break; 
-    }
+    jQuery(function(){
+        var flag = false;
+        if(index >= 1 && updated_series.length > 3)
+        {
+            $("#" + updated_series[index + 2].book_id).attr("id",updated_series[index + 1].book_id); 
+            $("#" + updated_series[index + 1].book_id).attr("id",updated_series[index].book_id); 
+            $("#" + updated_series[index].book_id).attr("id",updated_series[index - 1].book_id); 
+            flag = true;  
+        }
+        if(flag && index >= 1)
+        {
+            $("#series-1").attr("src","images/" + updated_series[index - 1].image + ""); 
+            $("#series-2").attr("src","images/" + updated_series[index].image + "");  
+            $("#series-3").attr("src","images/" + updated_series[index + 1].image + "");
+            flag = false; 
+            index = index - 1;
+        } 
+    });
 }
 
 function showList(data){
@@ -108,7 +115,7 @@ function showList(data){
     $.each(data,function(index,book) {
         series.push(book);
         $('#row').append(
-        "<button type=\"button\" " + "onclick=\"showAll(this.id)\"" + 
+        "<button type=\"button\" " + "onclick=\"getOne(this.id)\"" + 
         "id=\"" + book.book_id + "\">" +
         "<div class=\"column\">" + 
         "<img src=\"images/" + book.image + "\">" +
@@ -132,17 +139,13 @@ function profile_menu() {
     }
 }
 
-function move_left() {
-    $('#series-1').src("images/\"" + data[0].image + "\"");
-    $('#series-2').src("images/\"" + data[1].image + "\"");
-    $('#series-3').src("images/\"" + data[2].image + "\"");
-}
 
-function showAll(this_id) {
+
+function getOne(this_id) {
     $("#container").html("");
     $("#container").load("focus_book.html");
     id = this_id;
-    findOne(); 
+    findOne();
 }
 
 function home() {
